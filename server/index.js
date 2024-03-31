f082180972fc6fee24f3dd2105087844f15bf428
Delphine Sintamour <delsint123@gmail.com>
2024-03-30 21:31:05 -0400

    init

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+// Create express app
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//routes
+
+// Port server is listening on
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  res.status(500).send('Something broke!');
+  console.error(err.stack);
+}); 
+
+export default app;
