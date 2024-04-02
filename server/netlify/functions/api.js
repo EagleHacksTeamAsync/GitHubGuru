d0f8dce7db9421ee1220a20f9ad86f9c4921cdc3
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-04-02 18:39:46 -0400

    added server side config for user authentication

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -8,6 +8,56 @@ const api = express();
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+api.use("/", router);
+
+//routes
+app.get("/getAccessToken", async (req, res) => {
+  console.log(req.query.code);
+
+  const params =
+    "? client_id=" +
+    CLIENT_ID +
+    "&client_secret=" +
+    CLIENT_SECRET +
+    "&code=" +
+    req.query.code;
+
+  await fetch("https://github.com/login/oauth/access_token" + params, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.get("/getUserData", async (req, res) => {
+  req.get("Authorization");
+  await fetch("https://api.github.com/user", {
+    method: "GET",
+    headers: {
+      Authorization: req.get("Authorization"),
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      res.send;
+    });
+});
 
 export const handler = serverless(api);
