6302a7f0e51b905bf1db3446ead069f6a2a980f3
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-04-05 15:46:01 -0400

    added user auth that works

--- extension/src/background/index.js
+++ extension/src/background/index.js
@@ -1,80 +1,73 @@
-function authenticateWithGitHub() {
+async function authenticateWithGitHub() {
     let clientId, clientSecret;
 
-    fetch("../config/config.json")
-        .then((response) => response.json())
-        .then((data) => {
-            clientId = data.clientId;
-            clientSecret = data.clientSecret;
-        })
-        .catch((error) => {
-            console.error("Error fetching config:", error);
-        });
+    try {
+        const response = await fetch("../config/config.json");
+        const data = await response.json();
+        clientId = data.client_id;
+        clientSecret = data.client_secret;
+    } catch (error) {
+        console.error("Error fetching config:", error);
+        return;
+    }
+
+    console.log(clientId, clientSecret);
 
     const redirectUri = chrome.identity.getRedirectURL();
     const scope = "read:user";
-    const authUrl = `https://github.com/login/oauth/authorize?client_id=ae74d81ecc346767a9bc&redirect_uri=${encodeURIComponent(
-        redirectUri
-    )}&scope=${encodeURIComponent(scope)}&response_type=token`;
+    const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&response_type=token`;
 
-    chrome.identity.launchWebAuthFlow(
-        {
-            url: authUrl,
-            interactive: true,
-        },
-        (redirectUrl) => {
-            if (redirectUrl) {
-                // Extract the access token from the redirect URL
-                const urlParams = new URLSearchParams(
-                    redirectUrl.split("?")[1] // Discard everything before the hash
-                );
+    console.log(authUrl);
+    console.log(redirectUri)
 
-                const code = urlParams.get("code");
+    chrome.identity.launchWebAuthFlow({
+        url: authUrl,
+        interactive: true,
+    }, async (redirectUrl) => {
+        if (!redirectUrl) {
+            if (chrome.runtime.lastError) {
+                console.log("Error or cancellation:", chrome.runtime.lastError.message);
+            }
+            return;
+        }
 
-                if (!code) {
-                    console.error("No code found in the redirect URL.");
-                    return;
-                }
+        const urlParams = new URLSearchParams(redirectUrl.split("?")[1]);
+        const code = urlParams.get("code");
 
-                const data = {
+        if (!code) {
+            console.error("No code found in the redirect URL.");
+            return;
+        }
+
+        try {
+            const response = await fetch('https://github.com/login/oauth/access_token', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
                     client_id: clientId,
                     client_secret: clientSecret,
                     code: code
-                };
-
-                fetch('https://github.com/login/oauth/access_token', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
                 })
-                    .then((response) => 
-                        response.json()
-                    )
-                    .then((data) => {
-                        const accessToken = data['access_token'];
+            });
 
-                        if(accessToken) {
-                            chrome.storage.local.set({ accessToken }, () => {
-                                console.log("Access token stored.");
-                            });
-                        }
-                        else {
-                            console.error("No access token found in the response.");
-                        }
-                    })
-                    .catch((error) => {
-                        console.error("Error fetching access token:", error);
-                    });
+            const responseData = await response.json();
+            console.log("GitHub response:", responseData)
+            const accessToken = responseData['access_token'];
 
-            } else if (chrome.runtime.lastError) {
-                // Log any errors encountered during the auth flow
-                console.log("Error or cancellation:", chrome.runtime.lastError.message);
+            if (accessToken) {
+                chrome.storage.local.set({ accessToken }, () => {
+                    console.log("Access token stored.");
+                });
+            } else {
+                console.error("No access token found in the response.");
             }
+        } catch (error) {
+            console.error("Error fetching access token:", error);
         }
-    );
+    });
 }
 
 
@@ -93,7 +86,7 @@ async function fetchEvents(accessToken) {
 }
 
 function updateIcon(hasNotifications) {
-    const iconPath = hasNotifications ? '../logo2.png' : '../logo.png';
+    const iconPath = hasNotifications ? '../logo-2.png' : '../logo.png';
     chrome.action.setIcon({ path: iconPath });
 }
 function testChromeNotification() {
@@ -113,7 +106,7 @@ function chromeNotification(accessToken) {
             chrome.notifications.create(
                 {
                     type: "basic",
-                    iconUrl: "logo2.png",
+                    iconUrl: "../logo-2.png",
                     title: "GitHub Events",
                     message: message,
                     silent: false
