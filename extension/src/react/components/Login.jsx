a11b0d5be9679731a45a879f554a12472bb79b45
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-04-05 14:50:28 -0400

    fixed font and logo on button

--- extension/src/react/components/Login.jsx
+++ extension/src/react/components/Login.jsx
@@ -11,8 +11,8 @@ const Login = ({ userData, onLogin, onLogout  }) => {
                 visible={isModalVisible}
                 onCancel={() => {}}
                 footer={[
-                    <Button key="login" type="primary" onClick={onLogin} style={{display:"inline-flex", alignItems:"center",}}>
-                        <img src="./images/logo-2.png" alt="GitHub" style={{ verticalAlign:"middle", marginRight:12, width:18 }} />
+                    <Button key="login" type="primary" onClick={onLogin} style={{ fontFamily: "Montserrat, sans-serif", fontWeight:"400", display:"inline-flex", alignItems:"center",}}>
+                        <img src="./images/logo-2.png" alt="GitHub" style={{ verticalAlign:"middle", marginRight:10, width:18, }} />
                         Login with GitHub
                     </Button>,
                 ]}
